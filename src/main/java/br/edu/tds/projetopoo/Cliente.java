31a830510b4d23765a8e5095eeb5969b962adf9c
Erick Victor Sebastião <erick.victor@ifsuldeminas.edu.br>
2026-02-23 14:14:19 -0300

    Criação de Cliente Java e inserção de dados

---
+++ src/main/java/br/edu/tds/projetopoo/Cliente.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.tds.projetopoo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cliente {
+    //definição de atributos da classe
+    private String nome;
+    private int idade;
+    private String cpf;
+    
+    //definir o(s) meu(s) construtores
+    public Cliente(){
+    
+    }
+    
+    public Cliente(String n, int i, String cpf){
+    this.nome = n;
+    this.idade = i;
+    this.cpf = cpf;        
+    }
+    
+    public void imprimeDadosCliente(){
+        System.out.println("nome: " + this.nome);
+        System.out.println("idade: " + this.idade);
+        System.out.println("cpf: " + this.cpf);
+    }
+    
+    
+}
---
+++ src/main/java/br/edu/tds/projetopoo/JavaPrincipal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.tds.projetopoo;
+
+/**
+ *
+ * @author aluno
+ */
+public class JavaPrincipal {
+    public static void main(String[] args){
+            System.out.println("Testando objetos da classe cliente");
+            //usando a classe cliente
+            Cliente c1 = new Cliente("Davi",19, "123456");
+            c1.imprimeDadosCliente();
+}
+}
